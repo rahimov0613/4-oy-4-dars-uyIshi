5b58405c550879b11385bad979b62273a5426e49
Abduhamid rahimov <rakhimov0613@gmail.com>
2024-12-21 17:53:16 +0500

    3 misol bajarildi

--- server.js
+++ server.js
@@ -8,46 +8,73 @@ let items = [
 
 
 const server = http.createServer((request, response) => {
-    if (request.method !== `GET`) {
+    if (request.method !== `GET` && request.method !== `POST`) {
         response.writeHead(405, { "content-type": "application/json" });
         return response.end(JSON.stringify({ message: `Bunday buyruqga ruxsatingiz yo'q` }))
+
     }
-
     const { pathname, query } = url.parse(request.url, true);
-    if (pathname === `/items`) {
-        let getPhones = items;
+    if (request.method === `GET`) {
 
-        if (query.brand) {
-            getPhones = getPhones.filter(phone => { phone.brand.toLowerCase() === query.brand.toLowerCase() })
-        }
+        if (pathname === `/items`) {
+            let getPhones = items;
 
-        if (query.maxPrice) {
-            const getPrice = parseFloat(query.maxPrice)
-            if (!isNaN(getPrice)) {
-                getPhones = getPhones.filter(phone => phone.price <= getPrice)
+            if (query.brand) {
+                getPhones = getPhones.filter(phone => { phone.brand.toLowerCase() === query.brand.toLowerCase() })
             }
-        }
 
-        response.writeHead(200, { "content-type": "application/json" });
-        response.end(JSON.stringify(getPhones));
+            if (query.maxPrice) {
+                const getPrice = parseFloat(query.maxPrice)
+                if (!isNaN(getPrice)) {
+                    getPhones = getPhones.filter(phone => phone.price <= getPrice)
+                }
+            }
+
+            response.writeHead(200, { "content-type": "application/json" });
+            response.end(JSON.stringify(getPhones));
 
 
-    } else if (pathname.startsWith(`/phones/`)) {
-        const id = pathname.split(`/`)[2]
-        const phone = items.find(item => item.id.toString() === id);
+        } else if (pathname.startsWith(`/phones/`)) {
+            const id = pathname.split(`/`)[2]
+            const phone = items.find(item => item.id.toString() === id);
 
-        if (phone) {
-            response.writeHead(200, { "content-type": `application/json` })
-            return response.end(JSON.stringify(phone))
+            if (phone) {
+                response.writeHead(200, { "content-type": `application/json` })
+                return response.end(JSON.stringify(phone))
+
+            } else {
+                response.writeHead(404, { "content-type": `application/json` })
+                return response.end(JSON.stringify({ message: `Telefon topilmadi!` }));
+            }
+        } else if (request.method === `POST` && pathname === `/phones`) {
+            let body = ``
+            request.on(`data`, chunk => {
+                body += chunk;
+            });
+            request.on(`end`, () => {
+                try {
+                    const newPhone = JSON.parse(body);
+                    if (!newPhone.name || !newPhone.brand || !newPhone.price || !newPhone.stock) {
+                        response.writeHead(400, { "content-type": `application/json` });
+                        return response.end(JSON.stringify({ message: `Banrcha malumotlarni bering` }))
+                    }
+
+                    const newId = items.length ? items[items.length - 1].id + 1 : 1
+                    const createPhone = { id: newId, ...newPhone }
+                    items.push(createPhone);
+
+                    response.writeHead(201, { "content-type": `application/json` })
+                    return response.end(JSON.stringify(createPhone))
+                } catch (error) {
+                    response.writeHead(400, { "content-type": `application/json` });
+                    return response.end(JSON.stringify({ message: `JSON da hatolik bor!!!!` }))
+                }
+            })
 
         } else {
             response.writeHead(404, { "content-type": `application/json` })
-            return response.end(JSON.stringify({ message: `Telefon topilmadi!` }));
+            return response.end(JSON.stringify({ message: `topilmadi!` }));
         }
-
-    } else {
-        response.writeHead(404, { "content-type": `application/json` })
-        return response.end(JSON.stringify({ message: `topilmadi!` }));
     }
 });
 server.listen(3000, () => {
